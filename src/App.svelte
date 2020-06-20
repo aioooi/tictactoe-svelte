f57a8b07abb833ce12985994b280957b656d7c4a
chris@prime <66511731+aioooi@users.noreply.github.com>
2020-06-20 10:30:57 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  import Game from './Game.svelte';
+
+  export let name;
+  
+  let handicap = 1002936;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<Game {handicap}/>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 400px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Game.svelte
@@ -0,0 +1,73 @@
+<script>
+  import { onMount } from "svelte";
+
+  export let handicap = 100;
+
+  const EMPTY = 0;
+  const HUMAN = 1;
+  const COMPUTER = -1;
+
+  // let state = [[...Array(3)].map(() => [...Array(3)].map(() => 0))];
+  let state = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+  ]
+
+  function playField(i, j) {
+    state[i][j] = HUMAN;
+    console.log(state);
+  }
+</script>
+
+<style>
+  div {
+    padding: 2eM;
+    background-color: pink;
+  }
+  table {
+    margin: 0 auto; /* center the able with auto left right margin */
+    border-collapse: collapse;
+    border: 1px solid #656565;
+  }
+
+  tr {
+    margin: 0;
+    padding: 0;
+  }
+
+  td {
+    width: 1eM;
+    height: 1eM;
+    font-size: 3eM;
+    line-height: 1eM;
+    border: 3px solid #c4c4c4;
+    margin: 0;
+    padding: 1em;
+    cursor: pointer;
+  }
+
+  .played {
+    background-color: #e9e9e9;
+    cursor: auto;
+  }
+</style>
+
+<div>
+  <p>Handicap = {handicap}</p>
+  <table>
+    {#each [...Array(3).keys()] as i} 
+      <tr>
+        {#each [...Array(3).keys()] as j}
+          <td class:played={state[i][j] != 0}
+              on:click|once={() => playField(i, j)}>
+            <!-- <button on:click={() => playField(i, j)}>
+            </button> -->
+            {@html state[i][j] == HUMAN ? '&#x0fbe;' : ''}
+            {@html state[i][j] == COMPUTER ? '&#x262f;' : ''}
+          </td>
+        {/each}
+      </tr>
+    {/each}
+  </table>
+</div>
