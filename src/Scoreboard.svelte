e8866f014e15c30c7b0fd634931129e72f932ce6
chris@prime <66511731+aioooi@users.noreply.github.com>
2020-06-22 23:09:46 +0200

    grid layout

--- src/Scoreboard.svelte
+++ src/Scoreboard.svelte
@@ -2,27 +2,29 @@
   export let stats = {
     computer: 0,
     draw: 0,
-    player: 0,
-  }
+    player: 0
+  };
 
   export let name = "Player";
 </script>
 
 <style>
+  .grid {
+    display: grid;
+    text-align: center;
+    grid-template-columns: 1fr 1fr 1fr;
+    text-transform: uppercase;
+    margin: 3em 0;
+    grid-gap: 3px;
+  }
 
+  .cell {
+    padding: 0.3em;
+  }
 </style>
 
-<div>
-  <table>
-    <tr>
-      <td>{name}</td>
-      <td>Computer</td>
-      <td>Draws</td>
-    </tr>
-    <tr>
-      <td>{stats.player}</td>
-      <td>{stats.computer}</td>
-      <td>{stats.draw}</td>
-    </tr>
-  </table>
+<div class="grid">
+  {#each [name, 'Draw', 'Computer', stats.player, stats.draw, stats.computer] as t}
+    <div class="cell">{t}</div>
+  {/each}
 </div>
