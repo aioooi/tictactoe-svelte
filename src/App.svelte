de91aaf0590ed2487f3a02ab1e76774167079011
chris@prime <66511731+aioooi@users.noreply.github.com>
2020-06-24 14:58:22 +0200

    beta finished

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,18 @@
 <script>
-  import Game from './Game.svelte';
+  import Game from "./Game.svelte";
 </script>
 
-<main>
-	<Game/>
-</main>
-
 <style>
-	main {
+  main {
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-	}
+  }
 </style>
+
+<h1>Let's Play: Tic Tac Toe</h1>
+<h2>(implemented in Svelte)</h2>
+<main>
+  <Game />
+</main>
