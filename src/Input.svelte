67287abf658c5be87711b552ff17a4523a7d7314
chris@prime <66511731+aioooi@users.noreply.github.com>
2020-06-24 09:59:36 +0200

    pretty ;)

--- src/Input.svelte
+++ src/Input.svelte
@@ -7,14 +7,20 @@
     if (editOnlyOnce) {
       disabled = true;
     }
-  }
+  };
+
+  let focus = false;
+
+  const hidePencil = () => {
+    focus = true;
+  };
 </script>
 
 <style>
-  input[type=text] {
+  input[type="text"] {
     text-align: center;
     margin: 0;
-    width: 80%;
+    width: 95%;
     border: none;
     background: none;
     color: #777;
@@ -24,23 +30,40 @@
     margin-bottom: -2px;
   }
 
-  input[type=text]:focus {
+  input[type="text"]:focus {
     background: #fff5f5;
     outline: none;
     border-bottom: 2px solid #ffa7a7;
   }
 
-  input[type=submit] {
+  input[type="submit"] {
+    display: none;
+  }
+
+  label {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    padding-left: 0.7em;
+  }
+
+  .hidden {
     display: none;
   }
 </style>
 
 <form on:submit|preventDefault={handleInput}>
-{#if disabled}
-  <input type="text" bind:value disabled />
-{:else}
-  <label>&#x270E;</label>
-  <input type="text" bind:value on:focusout={handleInput}/>
-{/if}
-  <input type="submit">
+  {#if disabled}
+    <input type="text" bind:value disabled />
+  {:else}
+    <div style="position:relative">
+      <label class:hidden={focus}>&#x270E;</label>
+      <input
+        type="text"
+        bind:value
+        on:focusout={handleInput}
+        on:focus={hidePencil} />
+    </div>
+  {/if}
+  <input type="submit" />
 </form>
